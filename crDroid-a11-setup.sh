013663ea99c87ce43f8006ba6be6f5f7c37d80f2
develux <103586125+develux44@users.noreply.github.com>
2025-03-15 12:58:01 +0500

    Update crDroid-a11-setup.sh (Fix webview apk error)

--- crDroid-a11-setup.sh
+++ crDroid-a11-setup.sh
@@ -3,6 +3,13 @@
 repo init -u https://github.com/crdroid-security/android.git -b 11.0 --git-lfs --depth=1
 # Sync ROM source
 repo sync
+# Fix webview build error 
+rm -rf external/chromium-webview/prebuilt/*
+rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
+rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
+git lfs install
+git lfs install external/chromium
+repo sync
 # Device tree 
 git clone https://github.com/Xiaomi-SM6225-fog-development/android_device_xiaomi_fog-11.git device/xiaomi/fog
 # Kernel headers for prebuilt MIUI kernel
