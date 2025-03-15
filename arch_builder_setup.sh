0c9f461c9a30e8e48ce6d2731015cffc10c4b05a
develux <103586125+develux44@users.noreply.github.com>
2025-03-15 12:12:27 +0500

    Update arch_builder_setup.sh (add android-tools)

--- arch_builder_setup.sh
+++ arch_builder_setup.sh
@@ -1,3 +1,3 @@
 # Setup script to get all needed packages for building on Arch-based distros
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si
-yay -Syyuu android-devel wget repo less vi openssh
+yay -Syyuu android-devel wget repo less vi openssh android-tools
