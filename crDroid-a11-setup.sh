561d06b5c1a72e57d8763c19afa01db5153fb196
develux <103586125+develux44@users.noreply.github.com>
2025-05-13 21:34:37 +0500

    Update crDroid-a11-setup.sh

--- crDroid-a11-setup.sh
+++ crDroid-a11-setup.sh
@@ -8,8 +8,8 @@ rm -rf external/chromium-webview/prebuilt/*
 rm -rf .repo/projects/external/chromium-webview/prebuilt/*.git
 rm -rf .repo/project-objects/LineageOS/android_external_chromium-webview_prebuilt_*.git
 git lfs install
-git lfs install external/chromium
-repo sync
+git lfs install external/chromium-webview/prebuilt
+repo sync -j$(nproc --all)
 # Device tree 
 git clone https://github.com/Xiaomi-SM6225-fog-development/android_device_xiaomi_fog-11.git device/xiaomi/fog
 # Kernel headers for prebuilt MIUI kernel
